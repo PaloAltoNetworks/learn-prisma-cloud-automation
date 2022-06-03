e3e2597820ce36c6b4efb32e7c268d06b692ec00
jjchavanne <31355989+jjchavanne@users.noreply.github.com>
2022-06-02 23:51:45 -0400

    Create ami-versions.tf

---
+++ compute/custom-defender-images/deploy-1/ami-versions.tf
@@ -0,0 +1,15 @@
+# Get latest Ubuntu Linux Focal Fossa 20.04 AMI
+data "aws_ami" "ubuntu-linux-2004" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+  
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
