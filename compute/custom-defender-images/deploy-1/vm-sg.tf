e6161db87d0b49c60d222662a2c0724d540a1651
jjchavanne <31355989+jjchavanne@users.noreply.github.com>
2022-06-03 00:02:40 -0400

    Create vm-sg.tf

---
+++ compute/custom-defender-images/deploy-1/vm-sg.tf
@@ -0,0 +1,27 @@
+# Define the security group for the Linux server
+resource "aws_security_group" "aws-vm-sg" {
+  name        = "${var.vm_name}-sg"
+  description = "Allow incoming traffic to the EC2 Instance"
+  #vpc_id      = aws_vpc.vpc.id   #Optional if creating new VPC.  If commented out, will use default.
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming HTTPS/TLS connections"
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Define the Allowable SSH address(es)
+    cidr_blocks = var.ssh_cidr_blocks
+    description = "Allow incoming SSH connections"
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
